46fd78a154be6122f6c7700354a2cac8821716d1
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-06-20 22:38:39 +0300

    add tri_kita_part2_ex2.java - the exercise is not full yet

---
+++ tri_kita_part2_ex2.java
@@ -0,0 +1,124 @@
+import java.util.SortedMap;
+
+//описание класса "Подводные суда"
+class UnderWaterVessel {
+    private int depthOfImmersion; //глубина погружения
+    private int autonomyHours; //автономность в часах
+    private int displacement; //водоизмещение
+    private int radiusOfOperation; //радиус действия
+
+    //Описание параметризированного конструктора
+    UnderWaterVessel(int depthOfImmersion, int autonomyHours, int displacement, int radiusOfOperation) {
+        this.depthOfImmersion = depthOfImmersion;
+        this.autonomyHours = autonomyHours;
+        this.displacement = displacement;
+        this.radiusOfOperation = radiusOfOperation;
+    }
+
+    //геттеры класса - получаем значения изначально скрытых полей
+    public int getDepthOfImmersion() { return depthOfImmersion; }
+    public int getAutonomyHours() { return autonomyHours; }
+    public int getDisplacement() { return displacement; }
+    public int getRadiusOfOperation() { return radiusOfOperation; }
+    
+    //сеттеры класса - методы для установки новых значений для изначально скрытых полей
+    public void setDepthOfImmersion(int depthOfImmersion) { this.depthOfImmersion = depthOfImmersion; }
+    public void setAutonomyHours(int autonomyHours) { this.autonomyHours = autonomyHours; }
+    public void setDisplacement(int displacement) { this.displacement = displacement; }
+    public void setRadiusOfOperation(int radiusOfOperation) { this.radiusOfOperation = radiusOfOperation; }
+
+    //уникальные методы, присущие исключительно данному классу
+    public void upward() { System.out.println("Процесс всплытия запущен!"); }
+    public void downward() { System.out.println("Процесс погружения запущен!"); }
+
+    public void foo() {
+        System.out.println("Метод foo для класса UnderWaterVessel");
+    }
+}
+
+//описание класса DroneUW, который является потомком класса UnderWaterVessel
+class DroneUW extends UnderWaterVessel {
+    private String name; //имя дрона
+    private int cableLen; //длина кабеля
+    private boolean isIRsensor; //наличие ИК-датчика
+
+    //описание параметризированного класса с наследованием полей от родительского класса через конструкцию super()
+    DroneUW(int depthOfImmersion, int autonomyHours, int displacement, int radiusOfOperation, String name, 
+            int cableLen, boolean isIRsensor) {
+        super(depthOfImmersion, autonomyHours, displacement, radiusOfOperation);
+        this.name = name;
+        this.cableLen = cableLen;
+        this.isIRsensor = isIRsensor;
+    }
+    
+    //геттеры класса-потомка
+    public String getName() { return name; }
+    public int getCableLen() { return cableLen; }
+    public boolean getIsIRsensor() { return isIRsensor; }
+
+    //сеттеры класса-потомка
+    public void setName(String name) { this.name = name; }
+    public void setCableLen(int cableLen) { this.cableLen = cableLen; }
+    public void setIsIRsensor(boolean isIRsensor) { this.isIRsensor = isIRsensor; }
+
+    //уникальные методы класса-потомка
+    public void cableEject() {
+        System.out.println("Разматываем кабель!");
+    }
+
+    public void switchOnIRsensor() {
+        if (isIRsensor = true) System.out.println("ИК - режим активирован!");
+    }
+
+    public void foo() {
+        System.out.println("Метод foo для класса DroneUW");
+    }
+}
+
+//описание класса "Боевая подлодка", который является потомком класса "Подводные суда"
+class BattleSubmarine extends UnderWaterVessel {
+    private int torpedoesMax; //максимальное число торпед
+    private int bombMax; //максимальное число бомб
+    private int sonarRange; //зона действия сонара в метрах
+    private String model; //модель подлодки
+
+    //описание параметризированного конструктора для класса
+    BattleSubmarine(int depthOfImmersion, int autonomyHours, int displacement, int radiusOfOperation,
+                    int torpedoesMax, int bombMax, int sonarRange, String model) {
+        super(depthOfImmersion, autonomyHours, displacement, radiusOfOperation);
+        this.torpedoesMax = torpedoesMax;
+        this.bombMax = bombMax;
+        this.sonarRange = sonarRange;
+        this.model = model;
+    }
+
+    //геттеры для класса - получаем значения скрытых полей
+    public int getTorpedoesMax() { return torpedoesMax; }
+    public int getBombMax() { return bombMax; }
+    public int getSonarRange() { return sonarRange; }
+    public String getModel() { return model; }
+    
+    //сеттеры для класа - задаем значения скрытым полям
+    public void setTorpedoesMax(int torpedoesMax) { this.torpedoesMax = torpedoesMax; }
+    public void setBombMax(int bombMax) { this.bombMax = bombMax; }
+    public void setSonarRange(int sonarRange) { this.sonarRange = sonarRange; }
+
+    //уникальные методы для класса
+    public void torpedoeLaunch(){
+        System.out.println("Пуск торпеды!");
+    }
+
+    public void bombSet(){
+        System.out.println("Бомба установлена!");
+    }
+
+    public void foo(){
+        System.out.println("Метод foo для класса BattleSubmarine");
+    }
+}
+
+public class tri_kita_part2_ex2 {
+    public static void main(String args[]) {
+
+    }
+}
