93689e5da13f83ade26cdea1b4bd5603f171fc0e
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2022-06-26 12:50:09 +0300

    Delete filepathconstant.java

--- Files/filepathconstant.java
+++
@@ -1,6 +0,0 @@
-public class filepathconstant {
-    public static String getPath() {
-        final String FILE_PATH = "/media/dima/F462A22262A1E996/Personal/Coding/Java/1 - Шилдт/Github/Java_Learning_Shildt/Files/";
-        return FILE_PATH;
-    }
-}
