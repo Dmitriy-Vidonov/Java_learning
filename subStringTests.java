a70ac5719ba9527b581c58baaa221d9ada65346e
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-07-11 22:40:06 +0300

    update long string tests for subString search

--- subStringTests.java
+++ subStringTests.java
@@ -1,30 +1,38 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.io.IOException;
-
-import static org.junit.Assert.fail;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.util.concurrent.TimeUnit;
 
 class SubStrMethodTest {
 
     static String str1;
     static String str2;
+    String longString;
+    String subStr;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() throws IOException {
         str1 = StringGen.generator(1000);
         str2 = StringGen.generator(10);
+        longString = StringGen.generator(10000000);
+        subStr = StringGen.generator(5);
     }
 
     //regression test
     @org.junit.jupiter.api.Test
+    @DisplayName(" - regression test - \uD83D\uDE3A")
     public void regressionTest() throws IOException {
         Assertions.assertEquals(true, SubStrMethod.searchMethod("avangardnaya", "vanga"), "substring - vanga is in string - avangardnaya");
     }
 
     //random values test
-    @RepeatedTest(1000)
+    @RepeatedTest(10000)
+    @DisplayName(" - random values test - ╯°□°）╯")
     public void randomTest() throws Exception {
         String text = StringGen.generator((int) ((Math.random() * (50 - 10) + 1) + 10));
         String example = StringGen.generator((int) (Math.random() * 5));
@@ -36,8 +44,45 @@ class SubStrMethodTest {
         Assertions.assertEquals(text.contains(example), SubStrMethod.searchMethod(text, example), "Expect that str1 = " + text + " contains str2 = " + example);
     }
 
+    //long string test with jUnit timeout
+    @org.junit.jupiter.api.Test
+    @DisplayName(" - long string test with jUnit timeout - ( ಠ ʖ̯ ಠ )")
+    @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
+    public void longStringTestJunit() throws IOException {
+    try {
+        SubStrMethod.searchMethod(longString, subStr);
+    }
+    catch (Exception ex) {
+        System.out.println("Long string test error - " + ex);
+    }
+    }
+
+    //long string test with system timeout
+    @org.junit.jupiter.api.Test
+    @DisplayName(" - long string test with system timeout [¬º-°]¬")
+    public void longStringTestSystem() throws Exception{
+        long currTime = java.lang.System.currentTimeMillis();
+        try {
+            SubStrMethod.searchMethod(longString, subStr);
+        } catch (Exception ex) {
+            System.out.println("Exception in long string test with system timeout - " + ex);
+        }
+        long currTime2 = java.lang.System.currentTimeMillis();
+
+            if (currTime2 - currTime > 50) {
+                System.out.println("OS: " + System.getProperty("os.name") + "\nVersion: " + System.getProperty("os.version") +
+                        "\nArchitecture: " + System.getProperty("os.arch"));
+                System.out.println("Available processors (cores): "
+                        + Runtime.getRuntime().availableProcessors());
+
+                OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
+                System.out.println("System CPU load: " + operatingSystemMXBean.getSystemLoadAverage());
+            }
+        }
+
     //test with null values
     @org.junit.jupiter.api.Test
+    @DisplayName(" - test with null values - (⊙_⊙)")
     public void nullTest() throws IOException {
         Assertions.assertFalse(SubStrMethod.searchMethod(null, str2), "expect that null has no substring");
         Assertions.assertFalse(SubStrMethod.searchMethod(str1, null), "expect that there is no null in string");
@@ -46,6 +91,7 @@ class SubStrMethodTest {
 
     //empty values test
     @org.junit.jupiter.api.Test
+    @DisplayName(" - empty values test - ( ੭ ･ᴗ･ )੭")
     public void emptyTest() throws IOException {
         Assertions.assertEquals(SubStrMethod.searchMethod(str1, ""), true, "expecting that empty substring is in string");
         Assertions.assertEquals(SubStrMethod.searchMethod("", str2), false, "expecting that substring can't be inside empty string");
