91ccd120210683e2592181c1d79b6b51f76ffe87
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-07-21 08:20:54 +0300

    added comments to the code of printing directories list

---
+++ DirectoryParsing.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Печать всех подкаталогов текущего каталога до глубины вложенности 10*/
+        File root = new File("."); //задаем "отправную точку" в пути до папок - текущую директорию
+        ArrayList<File> expand = new ArrayList<>(); //expand - динамический массив с "путями" до каталогов
+        expand.add(root); //в "список путей" до каталогов добавили "." - текущую директорию
+
+        for (int depth = 0; depth < 10; depth++) { //повторяем проходку "вглубь" вплоть до вложенности 10
+            File[] expandCopy = expand.toArray(new File[expand.size()]); //в массив expandCopy помещаем содержимое списка expand
+            expand.clear(); //чистим список expand
+            for (File file : expandCopy) { //для каждого элемента массива expandCopy типа file
+                System.out.println(depth + " " + file); //печатаем текущий уровень глубины + содержимое элемента expandCopy
+
+                if (file.isDirectory()) {   //проверяем, имеется ли директория по адресу из file
+                    expand.addAll(Arrays.asList(file.listFiles())); //кладем в expand все, что находилось в file, т.е. список путей
+                }
+            }
+        }
+    }
+}
