c3dc75716316be9a38fb98de8877af28778e95c4
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-08-06 13:03:02 +0300

    add comments to 4_3 exercise

--- WorkWithDirsNFiles_ver1/src/main/java/Files_Dirs_4_3.java
+++ WorkWithDirsNFiles_ver1/src/main/java/Files_Dirs_4_3.java
@@ -5,15 +5,18 @@ public class Files_Dirs_4_3 {
         String res = "";
 
         try {
+            //проверка наличия субдиректории по размеру списка, в котором хранятся имена субдиректорий
             if (Files_Dirs_4_2.filesNdirs(dir, "*.*", false).get(1).size() == 0) {
-                int count = 0;
+                int count = 0; //переменная для инициализации цикла прохода по всем элементам списка файлов
                 count = (Files_Dirs_4_2.filesNdirs(dir, "*.*", false).get(0).size());
 
                 for (int i = 0; i < count; i++) {
+                    //проверка и удаление очередного элемента в списке файлов на удаление
                     if (new File(dir + "/" + Files_Dirs_4_2.filesNdirs(dir, "*.*", false).get(0).get(0)).exists()) {
                         new File(dir + "/" + Files_Dirs_4_2.filesNdirs(dir, "*.*", false).get(0).get(0)).delete();
                     }
                 }
+                //проверка существования директории после удаления файлов и ее удаление
                 if (new File(dir).exists()) {
                     new File(dir).delete();
                 }
