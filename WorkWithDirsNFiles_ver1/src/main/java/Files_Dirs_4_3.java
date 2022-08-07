65940aadb335a076172e0df7f7a1ebff0b3a0672
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-08-07 15:24:19 +0300

    update filesNdirsDel method

--- WorkWithDirsNFiles_ver1/src/main/java/Files_Dirs_4_3.java
+++ WorkWithDirsNFiles_ver1/src/main/java/Files_Dirs_4_3.java
@@ -2,26 +2,24 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Files_Dirs_4_3 {
-    public static String filesNDirsDel (String dir) {
-        String res = "";
+    public static boolean filesNDirsDel (String dir) {
+        boolean res = false;
         try {
-            ArrayList<ArrayList<String>> filesDirsList = new ArrayList<ArrayList<String>>();
-            filesDirsList = Files_Dirs_4_2.filesNdirs(dir, "*.*", false);
-            //проверяем длину списка, в котором хранятся имена подкаталогов. если = 0, значит он пуст и подпапок нет
-            if (filesDirsList.get(1).size() == 0) {
-                for (int i = 0; i < filesDirsList.get(0).size(); i++) {
-                    //удаляем файлы через проход элементов списка
-                    if (new File(dir + "/" + filesDirsList.get(0).get(i)).exists()) {
-                        new File(dir + "/" + filesDirsList.get(0).get(i)).delete();
+            if (new File(dir).exists()) {
+                ArrayList<ArrayList<String>> filesDirsList = new ArrayList<ArrayList<String>>();
+                filesDirsList = Files_Dirs_4_2.filesNdirs(dir, "*.*", false);
+                //проверяем длину списка, в котором хранятся имена подкаталогов. если = 0, значит он пуст и подпапок нет
+                if (filesDirsList.get(1).size() == 0) {
+                    for (int i = 0; i < filesDirsList.get(0).size(); i++) {
+                        //удаляем файлы через проход элементов списка
+                        if (new File(dir + "/" + filesDirsList.get(0).get(i)).exists()) {
+                            new File(dir + "/" + filesDirsList.get(0).get(i)).delete();
+                        }
                     }
-                }
-                //удаляем основную директорию
-                if (new File(dir).exists()) {
+                    //удаляем основную директорию
                     new File(dir).delete();
-                }
-                res = "Success!";
-            } else {
-                res = "Fail!";
+                res = true;
+               }
             }
         } catch (Exception ex) {
             System.err.println("Exception while trying to delete files and main directory - " + ex);
