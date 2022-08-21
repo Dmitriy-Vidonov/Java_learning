93d6b909d26b846446de30ddf2a30ef77d9a0d82
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-08-21 19:13:04 +0300

    add files and additional method for check correct work

---
+++ HashMap/src/test/java/hashNTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class hashNTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Regression test")
+    public void hashN() throws Exception {
+
+    }
+}
