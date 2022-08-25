9d9e638caa444c575b61f1606b4aaa423a1f3c9b
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2022-08-25 18:17:25 +0300

    Create CreateExcel.java

--- HashMap/src/main/java/CreateExcel.java
+++ HashMap/src/main/java/CreateExcel.java
@@ -1,9 +1,10 @@
-import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
@@ -11,17 +12,19 @@ public class CreateExcel {
     public static void main(String[] args) throws Exception {
         ArrayList<Integer> finList = new ArrayList<Integer>(ListGen.generateList(1, 10, 100));
         int N=10;
+        int totalRowsCount = 100;
+        String excelName = "workbook.xlsx";
         try {
             //Создание книги Excel
             XSSFWorkbook book = new XSSFWorkbook();
-            FileOutputStream fileout = new FileOutputStream("workbook.xlsx");
+            FileOutputStream fileout = new FileOutputStream(excelName);
 
             //Создание страницы
             XSSFSheet sheet = book.createSheet("Sheet 1");
 
             int rowsCount;
             //Создаем строки от 1 до 100. заполняем их значениями из массива
-            for (rowsCount = 0; rowsCount < 100; rowsCount++) {
+            for (rowsCount = 0; rowsCount < totalRowsCount; rowsCount++) {
                 XSSFRow row = sheet.createRow((short)rowsCount);
                 XSSFCell cell = row.createCell(0); //создали ячейку
                 cell.setCellType(Cell.CELL_TYPE_NUMERIC); //указали тип - числовой
@@ -44,13 +47,14 @@ public class CreateExcel {
 
                 //Проставили следующую партию формул - расчет числа вхождений чисел относительно параметра N
                 XSSFCell cell3 = row.createCell(2); //создали ячейку в 3-ем столбце
-                strFormula = "IF(A" + String.valueOf(100+i) + ">=" + N + ",TRUE())";
+                strFormula = "IF(A" + String.valueOf(totalRowsCount+i) + ">=" + N + ",TRUE())*1";
                 cell3.setCellType(Cell.CELL_TYPE_FORMULA); //указали тип - формула
                 cell3.setCellFormula(strFormula);
 
                 //Проставляем партию формул, где получаем числа, соответствующие числу вхождений >= N
                 XSSFCell cell4 = row.createCell(3); //создали ячейку в 4-ом столбце
-                strFormula = "IF(C" + String.valueOf(100+i) + "=1" + ",B" + String.valueOf(100+i) + ")";
+                strFormula = "IF(C" + String.valueOf(totalRowsCount+i) + "=1" + ",B"
+                        + String.valueOf(totalRowsCount+i) + ")*1";
                 cell4.setCellType(Cell.CELL_TYPE_FORMULA); //указали тип - формула
                 cell4.setCellFormula(strFormula);
             }
@@ -61,5 +65,13 @@ public class CreateExcel {
             System.out.println("An error during create excel file - " + ex);
         }
         //считать данные из последнего столбца формул и занести его в массив
+        try{
+            
+
+
+        } catch (Exception ex) {
+            System.out.println("Error during reading xlsx file - " + ex);
+        }
+
     }
 }
